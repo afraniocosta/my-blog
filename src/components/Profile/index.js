a71dbb9552801429d753c9ef9131427edecd791c
afraniocosta <afraniompc@gmail>
2020-04-09 20:48:35 -0300

    primeira pág criada, implementação do link

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'React'
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+      <h1>Afrânio</h1>
+      <h2>Desenvolvedor Front-End</h2>
+      <p>ASasAS asASasAS asASas</p>
+  </div>
+)
+
+export default Profile
